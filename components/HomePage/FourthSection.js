4136d1abe25e318f6086e840c60b9562073ab391
Quen Del Pilar <quen.delpilar@guerilla360.com>
2024-09-25 17:05:01 +0800

    Fix font sizes of the messages cards

--- components/HomePage/FourthSection.js
+++ components/HomePage/FourthSection.js
@@ -4,6 +4,12 @@ import Link from "next/link";
 const MESSAGES_ARRAY = [
   {
     id: 1,
+    title: "Our First Anniversary Together",
+    date: "Sept 23, 2024",
+    link: "/first-anniversary",
+  },
+  {
+    id: 1,
     title: "First Anniversary With You",
     date: "Sept 23, 2024",
     link: "/first-anniversary",
@@ -21,16 +27,16 @@ export default function FourthSection() {
           {MESSAGES_ARRAY.map((message) => (
             <div
               key={message.id}
-              className="w-1/3 p-1"
+              className="w-1/2 p-1"
             >
-              <div className="flex justify-between items-center flex-col bg-rose-200 rounded-md p-1 h-20">
-                <h3 className="text-[10px] text-center text-rose-800">{message.title}</h3>
-                <p className="text-[8px] font-light text-rose-800">{message.date}</p>
+              <div className="flex justify-between items-center flex-col bg-rose-200 rounded-md p-1 h-24">
+                <h3 className="text-sm text-center text-rose-800">{message.title}</h3>
+                <p className="text-xs font-light text-rose-800">{message.date}</p>
                 <Link
                   href={message.link}
-                  className="flex items-center justify-center text-xs gap-1 font-light text-[7px] bg-rose-300 rounded-md w-full text-rose-900 hover:bg-rose-400"
+                  className="flex items-center justify-center text-xs gap-1 font-light bg-rose-300 rounded-md w-full text-rose-900 hover:bg-rose-400 p-1"
                 >
-                  <Mail size={8} strokeWidth={1.3} />
+                  <Mail size={12} strokeWidth={1.3} />
                   Read
                 </Link>
               </div>
