9bedff62c322a867a34436e790e8b91988ea1a03
Quen Del Pilar <quendelpilar07@gmail.com>
2025-01-03 11:49:54 +0800

    Remove test comment

--- app/page.js
+++ app/page.js
@@ -1,7 +1,5 @@
 "use client";
 
-// Testing new device *Random Comment*
-
 import HomePage from "@/components/HomePage";
 import InitialAnimation from "@/components/InitialAnimation";
 import PageTransition from "@/utilities/PageTransition";
